c6e76f0eb8d2c3211a9917f185ba42ae4d1d0fab
ivanrevandia <ivanrevandia@gmail.com>
2017-09-18 17:47:13 +0700

    Project

---
+++ pindah_scene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pindah_scene : MonoBehaviour {
+
+	// Use this for initialization
+	public void pindahkescene (string pindahke){
+		Application.LoadLevel (pindahke); 
+	}
+}
